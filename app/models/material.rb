950e9afea07fc468b4acf6c7ef04b5b4ed4b7ac9
Harriet Craven <hc6@sanger.ac.uk>
2017-01-19 16:03:42 +0000

    refactored material client

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,22 +1,28 @@
 class Material
 
-  def self.site
-    Faraday.new(url: Rails.configuration.materials_service_url) do |faraday|
-      faraday.use ZipkinTracer::FaradayHandler, 'eve'
-      faraday.request :url_encoded
-      faraday.adapter Faraday.default_adapter
-    end
-  end
-
   def self.valid?(uuids)
-    response = Material.site.post do |req|
+    conn = get_connection
+
+    response = conn.post do |req|
       req.url '/materials/validate'
       req.headers['Content-Type'] = 'application/json'
       req.headers['Accept'] = 'application/json'
       req.body = { materials: uuids }.to_json
     end
-
     response.body == 'ok'
+  end
+
+  private
+
+  def self.get_connection
+    conn = Faraday.new(:url => 'http://localhost:5000') do |faraday|
+      # faraday.use ZipkinTracer::FaradayHandler, 'eve'
+      faraday.request  :url_encoded             
+      faraday.response :logger                  
+      faraday.adapter  Faraday.default_adapter 
+      faraday.proxy 'http://localhost:5000'
+    end
+    conn
   end
 
 end
